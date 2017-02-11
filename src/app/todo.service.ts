44619db91a1efeb3e342f070280b7a21487b5156
deliman2016 <jojoramdel@gmail.com>
2017-02-11 08:09:33 +0800

    char

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -3,10 +3,57 @@ import {Todo} from './shared/todo.model'
 
 @Injectable()
 export class TodoService {
-todos: Todo[] = [];
+
+ // placeholder  for last id  so we can simulate
+ // automatic incrementing if id's
+
+ lastID: number = 0;
+
+ // placeholder for todo's
+ todos: Todo[] = [];
   constructor() { 
 
   }
+
+/*// simulate POST/ todo
+addTodo(todo: Todo): TodoService {
+  if (!todo.id){
+    todo.id =++this.lastID;
+  }
+  this.todos.push(todo);
+  return this;
+}
+
+// Simulate PUT /todos/:id
+updateTodoByID(id: number, values: Object = {}): Todo{
+  let todo = this.getTodoById(id);
+  if(!todo){
+    return null;
+  }
+  Object.assign(todo, values);
+  return todo;
+}
+
+// Simulate GET / todos
+getAllTodos(): Todo[]{
+  return this.todos;
+}
+
+// Simulate GET / todos/ :id
+getTodoByid(id: number): Todo{
+  return this.todos
+    .filter(todo => todo.id === id)
+    .pop();
+}
+
+// toggle todo complete
+toggleTodoComplete(todo: Todo){
+  let updatedTodo = this.updateTodoByID(todo.id, {
+    complete: !todo.complete
+  });
+  return updatedTodo;
+}
+*/
 
 
 // get all todos
@@ -37,6 +84,7 @@ todos: Todo[] = [];
   	}
   }
 
+// toggle todo
   toggleTodoComplete(todo:Todo){
   	todo.completed = !todo.completed;
   	return this.put(todo);
